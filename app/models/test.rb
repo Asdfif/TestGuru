1369fd939a03719a757416e062f6cabcbb10381b
Asdfif <sasha-burov@mail.ru>
2021-01-15 17:18:04 +0300

    Созданы сиды, инстанс-метод в модели User, который возвращает список тестов, которые проходил юзер, создан метод класса в модели Test, возвращающий отсортированный массив названий всех тестов, у которых категория называется определенным образом.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.category_sort_desc(category_title)
+    joins("INNER JOIN categories ON categories.id = tests.category_id").where("categories.title = ?", category_title).order(title: :desc).pluck(:title)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db:seed command (or first_or_created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+c1 = Category.first_or_create(title: 'c1')
+c2 = Category.first_or_create(title: 'c2')
+
+u1 = User.first_or_create(name: 'U1')
+u2 = User.first_or_create(name: 'U2')
+
+t1 = Test.first_or_create(title: 't1', category_id: c1.id, author_id: u1.id)
+t2 = Test.first_or_create(title: 't2', category_id: c2.id, author_id: u1.id)
+
+q1 = Question.first_or_create(title: 'q1', test_id: t1.id)
+q2 = Question.first_or_create(title: 'q2', test_id: t2.id)
+
+a1 = Answer.first_or_create(title: 'a1', question_id: q1.id)
+a2 = Answer.first_or_create(title: 'a2', question_id: q2.id)
+
+u1t1 = UserTest.first_or_create(user_id: u1.id, test_id: t1.id)
+u1t2 = UserTest.first_or_create(user_id: u1.id, test_id: t2.id)
