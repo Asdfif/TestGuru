fa577761d3950957abc592703ab8522c3673709b
Asdfif <sasha-burov@mail.ru>
2021-01-16 16:07:22 +0300

    Исправлен сид, отформатированы методы для User и Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+if Test.all.empty?
+  Category.first_or_create([
+    {title: 'c1'},
+    {title: 'c2'}
+  ])
 
-c1 = Category.first_or_create(title: 'c1')
-c2 = Category.first_or_create(title: 'c2')
+  User.first_or_create([
+    {name: 'U1'},
+    {name: 'U2'}
+  ])
 
-u1 = User.first_or_create(name: 'U1')
-u2 = User.first_or_create(name: 'U2')
+  Test.first_or_create([
+    {title: 't1', category_id: 1, author_id: 1},
+    {title: 't2', category_id: 2, author_id: 2}
+  ]) 
 
-t1 = Test.first_or_create(title: 't1', category_id: c1.id, author_id: u1.id)
-t2 = Test.first_or_create(title: 't2', category_id: c2.id, author_id: u1.id)
+  Question.first_or_create([
+    {title: 'q1', test_id: 1},
+    {title: 'q2', test_id: 1},
+    {title: 'q3', test_id: 1},
+    {title: 'q4', test_id: 2},
+    {title: 'q5', test_id: 2},
+    {title: 'q6', test_id: 2},
 
-q1 = Question.first_or_create(title: 'q1', test_id: t1.id)
-q2 = Question.first_or_create(title: 'q2', test_id: t2.id)
+  ])
 
-a1 = Answer.first_or_create(title: 'a1', question_id: q1.id)
-a2 = Answer.first_or_create(title: 'a2', question_id: q2.id)
+  Answer.first_or_create([
+    {title: 'a1', question_id: 1, correct: true},
+    {title: 'a2', question_id: 1},
+    {title: 'a3', question_id: 2},
+    {title: 'a4', question_id: 2, correct: true},
+    {title: 'a5', question_id: 3},
+    {title: 'a6', question_id: 3},
+    {title: 'a7', question_id: 3, correct: true},
+    {title: 'a8', question_id: 4, correct: true},
+    {title: 'a9', question_id: 4},
+    {title: 'a10', question_id: 4},
+    {title: 'a11', question_id: 5},
+    {title: 'a12', question_id: 5, correct: true},
+    {title: 'a13', question_id: 6},
+    {title: 'a14', question_id: 6, correct: true},
 
-u1t1 = UserTest.first_or_create(user_id: u1.id, test_id: t1.id)
-u1t2 = UserTest.first_or_create(user_id: u1.id, test_id: t2.id)
+  ])
+
+  UserTest.first_or_create([
+    {user_id: 1, test_id: 1},
+    {user_id: 1, test_id: 2}
+  ])
+end
