25b1fab644c660a8e2ab403cd4095c1801c020db
Asdfif <sasha-burov@mail.ru>
2021-01-21 11:53:28 +0300

    Отрефакторены методы. Исправлено оформление. Заново создана таблица tests_users. К связям has_* добавлена опция dependent: :destroy

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,15 +1,14 @@
 class Test < ApplicationRecord
 
-  has_many :questions
   belongs_to :category
-  has_many :tests_users
-  has_many :users, through: :tests_users
-
   belongs_to :author, class_name: "User", foreign_key: "author_id"
+  has_many :questions, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
+  has_many :users, through: :tests_users, dependent: :destroy
 
   def self.category_sort_desc(category_title)
-    joins("INNER JOIN categories ON categories.id = tests.category_id")
-    .where("categories.title = ?", category_title)
+   joins(:category)
+    .where(category: {title: category_title})
     .order(title: :desc)
     .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
 
-  has_many :tests_users
-  has_many :tests, through: :tests_users
-  has_many :own_tests, class_name: "Test", foreign_key: "author_id"
+  has_many :tests_users, dependent: :destroy
+  has_many :tests, through: :tests_users, dependent: :destroy
+  has_many :own_tests, class_name: "Test", foreign_key: "author_id", dependent: :destroy
  
   def completed_tests(level)
-    Test.joins("JOIN tests_users ON tests_users.test_id = tests.id")
-    .where("tests.level = ?", level)
-    .where("tests_users.user_id = ?", self.id)
+    Test.joins(:tests_users)
+    .where(tests: {level: level})
+    .where(tests_users: {user_id: self.id})
   end
 
 end
