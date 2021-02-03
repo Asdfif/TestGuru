0d96c28ca8a7540da89df17f0c4fcabe00b94bff
Asdfif <sasha-burov@mail.ru>
2021-02-03 21:44:30 +0300

    Назначение следующего вопроса в модели TestPassage с помощью обратного вызова

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,13 +4,12 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, optional: true , class_name: "Question"
 
   before_validation :before_validation_set_first_question, on: :create
-
+  before_update :before_update_next_question
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
-    
-    self.current_question = next_question
+
     save!
   end
 
@@ -34,8 +33,8 @@ class TestPassage < ApplicationRecord
     current_question.answers.correct
   end
 
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+  def before_update_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
 end
