25fccf26b95c729a712b1fe8698d374d4be96d39
Asdfif <sasha-burov@mail.ru>
2021-02-08 11:59:28 +0300

    Убран лишний byebug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,12 @@
 class SessionsController < ApplicationController
 
-  def new
-
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:email])
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      byebug
       redirect_to  cookies[:required_path] || root_path
       cookies[:required_path] = nil
     else
