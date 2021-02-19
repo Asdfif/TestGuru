9f356d1019c5496f5143d4e304c71d3a8309496c
Asdfif <sasha-burov@mail.ru>
2021-02-19 13:09:36 +0300

    Исправлена ошибка JS возникающая при попытке изменить тест в полной форме на невалидный

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,13 +1,23 @@
 document.addEventListener('turbolinks:load', function() {
-  $('.form-inline-link').on('click', formInLineLinkHandler)
 
-  const errors = document.querySelector('.resource-errors')
+  const formInLine = document.querySelector('.form-inline-link')
 
-  if (errors) {
-    const resourceId = errors.dataset.resourceId
+  if (formInLine) {
 
-    formInLineHandler(resourceId)
-  } 
+    let links = document.querySelectorAll('.form-inline-link')
+
+    links.forEach(function(link) {
+      link.addEventListener('click', formInLineLinkHandler )
+    })
+
+    const errors = document.querySelector('.resource-errors')
+
+    if (errors) {
+      const resourceId = errors.dataset.resourceId
+
+      formInLineHandler(resourceId)
+    }
+  }
 })
 
 function formInLineLinkHandler(event) {
