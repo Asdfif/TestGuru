6b735f5311dc20419b78608a3c03dd72d14fe6d8
Asdfif <sasha-burov@mail.ru>
2021-02-24 23:43:48 +0300

    Убраны лишние комментарии

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,16 +24,6 @@ class User < ApplicationRecord
     .where(tests_users: { user_id: id })
   end
 
-  # def badge_deserved
-  #   user_successful_tests_ids = test_passages.where(success: true).pluck(:test_id)
-  #   Badge.all.each { |badge|
-  #     if (badge.tests_ids - user_successful_tests_ids).empty?
-  #       byebug
-  #       self.badges << badge
-  #     end
-  #   }
-  # end
-
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
   end
